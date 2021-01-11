b71e0423670f7404cecabb21e1c5cdfa6428e221
ItsZiroy <44354000+ItsZiroy@users.noreply.github.com>
2021-01-11 16:57:13 +0100

    added decryption

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import Modulocr from '../utils/modulocr';
 
-console.log(Modulocr.encrypt('rm')); // edge case
+console.log(Modulocr.encrypt('test')); // edge case
 
-console.log(Modulocr.encrypt('Informatik ist toll kappa'))
+console.log(Modulocr.encrypt('Informatik ist toll kappa'));
 
 for (let i = 0; i < 10; i++) {
-    console.log(Modulocr.encrypt('Informatik ist toll kappa'))
+    console.log(Modulocr.encrypt('Informatik ist toll kappa'));
 }
 
+// 3-afcyffdgs zat wzvc ddfii
+console.log(Modulocr.decrypt('1ijbarbbklp ?jz -?qa kkbcc'));
--- utils/modulocr.ts
+++ utils/modulocr.ts
@@ -1,5 +1,4 @@
 const index = {
-    0: 0,
     a: 1,
     b: 2,
     c: 3,
@@ -26,31 +25,36 @@ const index = {
     x: 24,
     y: 25,
     z: 26,
+    '-': 27,
+    '?': 28,
 };
 export default class Modulocr {
     static encrypt = (encryptable: string): string => {
-        const seed = parseInt((Math.random() * 10).toFixed(0)) || 1;
+        const seed = parseInt((Math.random() * 9).toFixed(0)) || 1;
 
         const parts = encryptable.toLowerCase().split('');
 
         let before = 0;
 
-        let result :string = '' + seed;
+        let result: string = '' + seed;
 
         parts.forEach((value: string) => {
-            if(value === ' ') {
+            if (value === ' ') {
                 result = result + ' ';
             } else {
-            const encrypted = Modulocr.encryptInt(
-                before,
-                value as keyof typeof index,
-                seed
-            );
-            result =
-                result +
-                Object.keys(index).find((key:string) => index[key as  keyof typeof index] === encrypted);
+                const encrypted = Modulocr.encryptInt(
+                    before,
+                    value as keyof typeof index,
+                    seed
+                );
+                result =
+                    result +
+                    Object.keys(index).find(
+                        (key: string) =>
+                            index[key as keyof typeof index] === encrypted
+                    );
 
-            before = encrypted;
+                before = encrypted;
             }
         });
         return result;
@@ -62,9 +66,63 @@ export default class Modulocr {
         seed: number
     ): number => {
         if (!before) {
-            return (index[encryptable] * seed) % 26;
+            return (index[encryptable] * seed) % 29;
         } else {
-            return (before * index[encryptable]) % 26;
+            return (before * index[encryptable]) % 29;
         }
+    };
+
+    static decrypt = (decryptable: string): string => {
+        const seed = parseInt(decryptable.split('')[0]);
+
+        const parts = decryptable.slice(1).split('');
+
+        let before = 0;
+
+        let result = '';
+
+        parts.forEach((value: string) => {
+            if (value === ' ') {
+                result = result + ' ';
+            } else {
+                const decrypted = Modulocr.decryptInt(
+                    before,
+                    value as keyof typeof index,
+                    seed
+                );
+
+                result =
+                    result +
+                    Object.keys(index).find(
+                        (key: string) =>
+                            index[key as keyof typeof index] === decrypted
+                    );
+
+                before = index[value as keyof typeof index];
+            }
+        });
+
+        return result;
+    };
+
+    static decryptInt = (
+        before: number,
+        decryptable: keyof typeof index,
+        seed: number
+    ): number => {
+        if (!before) {
+            before = seed;
+        }
+        let e = 0;
+
+        for (let i = 0; i < 30; i++) {
+            if (
+                Number.isInteger(((index[decryptable] + 29 * i) / before) % 29)
+            ) {
+                e = i;
+                break;
+            }
+        }
+        return (index[decryptable] + 29 * e) / before;
     };
 }
